25244446c07c21594ea3dddec999554f910adc7c
vini-boop-debug <vinimoraesaba@gmail.com>
2026-08-03 20:15:32 -0300

    Criado método listarProdutosVendidos

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -85,7 +85,42 @@ public ArrayList<ProdutosDTO> listarProdutos(){
     return listagem;
 }
     
-    
+    public ArrayList<ProdutosDTO> listarProdutosVendidos(){
+
+    ArrayList<ProdutosDTO> vendidos = new ArrayList<>();
+
+    String sql = "SELECT * FROM produtos WHERE status='Vendido'";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+
+        resultset = prep.executeQuery();
+
+        while(resultset.next()){
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            vendidos.add(produto);
+
+        }
+
+    } catch(Exception e){
+
+        JOptionPane.showMessageDialog(null, 
+            "Erro ao listar produtos vendidos: " + e.getMessage());
+
+    }
+
+    return vendidos;
+}
         
 }
 
